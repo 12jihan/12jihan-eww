8af186c0845e850da12609d9060cdd6440b1455e
12jihan <jareem.e.hoff@gmail.com>
2025-03-14 20:12:41 -0400

    made some more changes for the bar - might ext it to have more options later down the line

---
+++ scripts/active_window.sh
@@ -0,0 +1,22 @@
+#!/bin/env zsh
+
+# Get active window information
+window_info=$(hyprctl activewindow -j)
+
+# Extract title and class
+info_json=$(echo $window_info | jq)
+title=$(echo $window_info | jq -r '.title')
+class=$(echo $window_info | jq -r '.class')
+class=${class##*.}
+class="${(C)class}"
+
+# Check if title is empty and if "title" argument is passed
+if [[ "$1" == "title" ]]; then
+  if [[ -n "$title" ]]; then
+    echo $title
+  else
+    echo $class
+  fi
+else
+  echo $info_json
+fi
